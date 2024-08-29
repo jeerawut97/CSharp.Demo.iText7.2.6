1d7e1a0b415203e9b4677fbc23f3bdb43b8bf1d9
jeerawut <jeerawut.jd40@gmail.com>
2024-08-29 16:23:43 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace HelloPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var document = new Document(new PdfDocument(new PdfWriter("helloworld-pdf.pdf")));
+            document.Add(new Paragraph("Hello World!"));
+            document.Close();
+        }
+    }
+}
